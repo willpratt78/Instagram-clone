7ab3bdb69582dfc12501177191e8cad9ea63a2df
William Pratt <willpratt78@gmail.com>
2022-07-13 16:35:40 -0700

    WIP timestamp 1:05:40 Switch is now Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+
+const Login = lazy(()=> import ('./pages/login'))
+
 function App() {
   return (
-    <p>Hello World</p>
+    <Router>
+      <Suspense fallback={<p>Loading</p>}>
+        <Routes>
+          <Route path="/login" component={Login} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
