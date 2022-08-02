935f1e887ec0df337f9da6ef868178c8cfee62ad
William Pratt <willpratt78@gmail.com>
2022-08-02 14:52:47 -0700

    login page has phone image, tailwind working

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,5 +18,14 @@ export default function Login() {
     document.title = 'Login - Instagram';
   }, []);
 
-  return (<p>I am the login page!</p>)
+  return (
+    <div class="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div class="flex w-3/5">
+        <img src='./images/iphone-with-profile.jpg' alt="image of user logged in on phone" />
+      </div>
+      <div class="flex flex-col w-2/5">
+        <p>I will be the form</p>
+      </div>
+    </div>
+  )
 }
