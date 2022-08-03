aba2e76f8b122a35cca21f081bdd1b95fd225185
William Pratt <willpratt78@gmail.com>
2022-08-03 00:29:38 -0700

    login page almost complete vide 2:04

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,5 @@
 import { useState, useContext, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
@@ -24,33 +24,39 @@ export default function Login() {
         <img src='./images/iphone-with-profile.jpg' alt="image of user logged in on phone" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center-w-full">
-          <img src="./images/logo.png" alt="Instagram logo" className="at-2 w-6/12 ab-4" />
-        </h1>
-        {error && <p className="ab=4 text-xs text-red-primary">{error}</p>}
+        <div className='flex flex-col items-center bg-white p-4 border-border-gray-primary mb-4 rounded'>
+          <h1 className="flex justify-center-w-full">
+            <img src="./images/logo.png" alt="Instagram logo" className="at-2 w-6/12 ab-4" />
+          </h1>
+          {error && <p className="ab=4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-          aria-label="Enter your email address"
-          type="text"
-          placeholder='Email address'
-          className="text-sm text-gray-base m-full mr-3 py-5 px-4 h-2 border-border-fray-primary rounded ab-2"
-          onChange={({target}) => setEmailAddress(target.value)}
-          />
-           <input
-          aria-label="Enter your password"
-          type="password"
-          placeholder='Password'
-          className="text-sm text-gray-base m-full mr-3 py-5 px-4 h-2 border-border-fray-primary rounded ab-2"
-          onChange={({target}) => setPassword(target.value)}
-          />
-          <button 
-          disabled={isInvalid}
-          type="submit"
-          className={'bg-blue-500 text-red w-full rounded h-8 font-bold'}>
-          Login
-          </button>
-        </form>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder='Email address'
+            className="text-sm text-gray-base m-full mr-3 py-5 px-4 h-2 border-border-fray-primary rounded ab-2"
+            onChange={({target}) => setEmailAddress(target.value)}
+            />
+            <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder='Password'
+            className="text-sm text-gray-base m-full mr-3 py-5 px-4 h-2 border-border-fray-primary rounded ab-2"
+            onChange={({target}) => setPassword(target.value)}
+            />
+            <button 
+            disabled={isInvalid}
+            type="submit"
+            className={`bg-blue-500 text-black w-full rounded h-8 font-bold 
+            ${isInvalid && 'opacity-50'}`}>
+            Login
+            </button>
+          </form>
+        </div>
+        <div className='flex justify-center items-center flex-col w-full bg-white p-4 border-border-gray-primary'>
+          <p className='text-sm'>Don't have an account?{` `} <Link to="/signup" className="font-bold text-blue-medium">Sign Up</Link></p>
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './pages/**/*.{html,js}',
     './components/**/*.{html,js}',
     './index.html'
   ],
+  theme: {
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#005c98',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      }
+    }
+  }
 };
